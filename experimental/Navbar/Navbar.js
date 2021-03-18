4cbc5c78cb6c144714780d68506b8eb6c79811f6
Indiealistic <hey@indiealistic.studio>
2021-03-18 20:17:32 +0700

    Temporary menu

---
+++ experimental/Navbar/Navbar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import './Navbar.css'
+
+// Use this navbar to add any quick links (for an extension)
+const Navbar = () => {    
+    return(
+        <nav id="navbarExperimental">
+            <div id="contactButtons" >
+                <a href="https://github.com/indiealistic" target="_blank" className="item">
+                  <img src={require("../../public/github.png")} className="imageItem"/>
+                </a>
+                <a href="https://www.youtube.com/" target="_blank" className="item">
+                  <img src={require("../../public/youtube.png")} className="imageItem"/>
+                </a>
+                <a href="https://www.twitch.tv/" target="_blank" className="item">
+                  <img src={require("../../public/twitch.png")} className="imageItem"/>
+                </a>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar; 
